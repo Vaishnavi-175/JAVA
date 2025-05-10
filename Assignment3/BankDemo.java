1ce3c33b50a85fa83284a14fa4d324d995d9b786
Vaishnavi Gavade <vaishnavigavade17@gmail.com>
2025-05-10 20:23:16 +0530

    Add files via upload

---
+++ Assignment3/BankDemo.java
@@ -0,0 +1,25 @@
+class BankAccount {
+    protected double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+        }
+    }
+
